a4dd4bfcffd180fc24c3aec67b170df79fafbca0
1001MJH <1001moon@hanafos.com>
2025-05-22 17:42:53 +0900

    20250522

---
+++ SpringBootJReactHookProject_1/src/main/java/com/sist/web/SpringBootJReactHookProject1Application.java
@@ -0,0 +1,13 @@
+package com.sist.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJReactHookProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJReactHookProject1Application.class, args);
+	}
+
+}
