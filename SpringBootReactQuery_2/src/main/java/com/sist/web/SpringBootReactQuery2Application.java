b9648efdb44e920b51e2a698ac0f0fd7fad9c949
1001MJH <1001moon@hanafos.com>
2025-06-04 09:54:56 +0900

    20250604

---
+++ SpringBootReactQuery_2/src/main/java/com/sist/web/SpringBootReactQuery2Application.java
@@ -0,0 +1,16 @@
+package com.sist.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// 서버 구도 ㅇ=> Spring - App
+// 환경 설정 => XML => properties, yml
+// 버전 관리 / 라이브러리 관리 / 프로젝트 관리 => maven / gradel
+// 형상 관리 : GIT, SVN
+@SpringBootApplication
+public class SpringBootReactQuery2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactQuery2Application.class, args);
+	}
+
+}
